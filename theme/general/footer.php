4981617f6b8fba2d8d5cd65d0540875ed8e84ef7
HOME\Andry <shapovala@ukr.net>
2018-03-11 19:16:38 +0200

    add changes

--- theme/general/footer.php
+++ theme/general/footer.php
@@ -1,12 +1,18 @@
 <?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); } ?>
 <?php get_footer(); ?>
        <footer class="footer">
-           <!--
            <ul class="social">
-               <li><a rel="nofollow" target="_blank" href="http://vk.com/club28768096" title="vk"><i class="fa fa-vk"></i></a></li>
-               <li><a rel="nofollow" target="_blank" href="https://www.facebook.com/groups/404883239609152/" title="facebook"><i class="fa fa-facebook"></i></a></li>
+               <li>
+               		<a rel="nofollow" target="_blank" href="#" title="vk">
+               			<i class="fa fa-vk"></i>
+               		</a>
+               	</li>
+               <li>
+	               	<a rel="nofollow" target="_blank" href="#" title="facebook">
+	               		<i class="fa fa-facebook"></i>
+	               	</a>
+	            </li>
            </ul>
--->
            <div class="logo">
                <a href="<?php get_site_url(); ?>" title="">&nbsp;</a>
            </div>
@@ -14,29 +20,6 @@
            
            <div class="meter">
 		   <noindex>
-<!-- hit.ua -->
-<a href='http://hit.ua/?x=95112' target='_blank'>
-<script language="javascript" type="text/javascript"><!--
-Cd=document;Cr="&"+Math.random();Cp="&s=1";
-Cd.cookie="b=b";if(Cd.cookie)Cp+="&c=1";
-Cp+="&t="+(new Date()).getTimezoneOffset();
-if(self!=top)Cp+="&f=1";
-//--></script>
-<script language="javascript1.1" type="text/javascript"><!--
-if(navigator.javaEnabled())Cp+="&j=1";
-//--></script>
-<script language="javascript1.2" type="text/javascript"><!--
-if(typeof(screen)!='undefined')Cp+="&w="+screen.width+"&h="+
-screen.height+"&d="+(screen.colorDepth?screen.colorDepth:screen.pixelDepth);
-//--></script>
-<script language="javascript" type="text/javascript"><!--
-Cd.write("<img src='//c.hit.ua/hit?i=95112&g=0&x=4"+Cp+Cr+
-"&r="+escape(Cd.referrer)+"&u="+escape(window.location.href)+
-"' border='0' width='88' height='15' "+
-"alt='hit.ua: посетителей за сегодня' title='hit.ua: посетителей за сегодня'/>");
-//--></script></a>
-<!-- / hit.ua -->
-
 
 
 </noindex>
@@ -44,74 +27,17 @@ Cd.write("<img src='//c.hit.ua/hit?i=95112&g=0&x=4"+Cp+Cr+
 
            </div>
 
-		<?php /*  include('footer-text.php'); */ ?>
+		<?php   include('footer-text.php');  ?>
 		
 		
        </footer>
    </div>
    
-   <!-- Yandex.Metrika counter -->
- <script type="text/javascript">
-var yaParams = {ip_adress: "<? echo $_SERVER['REMOTE_ADDR'];?>"};
-//объявляем параметр ip_adress и записываем в него IP посетителя
-</script> 
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-    (function (d, w, c) {
-        (w[c] = w[c] || []).push(function() {
-            try {
-                w.yaCounter41784139 = new Ya.Metrika({
-                    id:41784139,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-                });
-            } catch(e) { }
-        });
-
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else { f(); }
-    })(document, window, "yandex_metrika_callbacks");
-</script>
-<!-- /Yandex.Metrika counter -->
 
 
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-10126728-12', 'auto');
-  ga('require', 'displayfeatures'); // это для ремаркетинга
-  
-  if (location.hostname == 'www.prostosait.com.ua') { // это для того, чтобы не ключил вебвизор
-      $.ajax({
-      url: '/getip.php',    // код этого скрипта ниже  
-      dataType : "html",                     
-      success: function (data, textStatus) {
-         ga('set', 'dimension1', data); // тут мы записали IP в переменную Google.Universal 
-         ga('send', 'pageview');
-      } 
-   });
-	setTimeout (function() { // тут мы каждые 15 секунд сообщаем гуглу, что человек на сайте. Чтобы процент отказов был ближе к Яндекс.Метрике
-		ga('send', 'event', 'New Visitor', location.pathname);
-	}, 15000);
-  } else {
-      ga('send', 'pageview');
-  }
 
-</script>
 
 
 <div id="popap-one" class="popap-wrap">
@@ -279,9 +205,6 @@ var yaParams = {ip_adress: "<? echo $_SERVER['REMOTE_ADDR'];?>"};
     </div>
 
 
-
-    <link rel="stylesheet" href="https://cdn.saas-support.com/widget/cbk.css">
-<script type="text/javascript" src="https://cdn.saas-support.com/widget/cbk.js?wcb_code=b0e4f212198e8840a6d2276b8801beda" charset="UTF-8" async></script>
     
 
 
